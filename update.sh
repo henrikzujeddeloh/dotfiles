e34ad923a2816448ad109581952b4fb69967cd17
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2023-03-21 14:08:51 +0100

    apt repo update in update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 
+read -p "would you like to update repos? " yn
+if[ "$answer" != "${answer#[Yy]}" ] ;then
+    apt update
+    apt upgrade
+else
+    echo "not updateing repos"
+fi
+
+
+
