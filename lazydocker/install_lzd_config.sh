1c951c1d0f94f87f392c73930082795a85339319
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2024-02-12 15:44:41 +0100

    updated: checks if config directory exists in ~/.config and creates it if necissary

--- lazydocker/install_lzd_config.sh
+++ lazydocker/install_lzd_config.sh
@@ -2,4 +2,10 @@
 
 rm -f $HOME/.config/lazydocker/config.yml
 
+# check if lazydocker config directory exists
+if [ ! -d $HOME/.config/lazydocker ]; then
+  mkdir -p $HOME/.config/lazydocker;
+fi
+
+# add symlinks
 ln -s $HOME/dotfiles/lazydocker/config.yml $HOME/.config/lazydocker/config.yml
--- neofetch/install_neofetch_config.sh
+++ neofetch/install_neofetch_config.sh
@@ -2,4 +2,10 @@
 
 rm -f $HOME/.config/neofetch/config.conf
 
+# check if neofetch config directory exists
+if [ ! -d $HOME/.config/neofetch ]; then
+  mkdir -p $HOME/.config/neofetch;
+fi
+
+# create symlinks
 ln -s $HOME/dotfiles/neofetch/config.conf $HOME/.config/neofetch/config.conf 
