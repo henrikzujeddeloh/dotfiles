fd8ac6b2b7e693c0ed830bf6917e739755b61790
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2024-11-14 19:52:01 +0100

    added: browse backup function

--- scripts/photon/backup.sh
+++ scripts/photon/backup.sh
@@ -6,8 +6,6 @@ SERVER=henrik@neutron.lan
 REPO=$PATH_TO_BACKUP/Backup/photon
 DATE=$(date +%Y%m%d)
 
-echo "Touch Yubikey"
-borg create --stats --progress --verbose $SERVER:$REPO::$DATE /Users/henrikzujeddeloh/
+sudo borg create --stats --progress --verbose $SERVER:$REPO::$DATE /Users/henrikzujeddeloh/
 
-echo "Touch Yubikey"
-borg prune --list --stats --keep-daily 7 --keep-weekly 4 --keep-monthly 12 $SERVER:$REPO
+sudo borg prune --list --stats --keep-daily 7 --keep-weekly 4 --keep-monthly 12 $SERVER:$REPO
