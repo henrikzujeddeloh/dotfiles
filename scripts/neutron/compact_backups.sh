5a1d87c242ab413883570de99a3c3c319a9ebf63
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2024-11-14 20:39:43 +0100

    updated: added off-site backup compacting to script

--- scripts/neutron/compact_backups.sh
+++ scripts/neutron/compact_backups.sh
@@ -7,7 +7,17 @@ set -e
 source $HOME/dotfiles/scripts/neutron/.env
 
 # compact all borg backup repositories
+echo "Compacting neutron backup"
 sudo borg compact $PATH_TO_BACKUP/Backup/neutron
+
+echo "Compacting proton backup"
 sudo borg compact $PATH_TO_BACKUP/Backup/proton
+
+echo "Compacting electron backup"
 sudo borg compact $PATH_TO_BACKUP/Backup/electron
+
+echo "Compacting photon backup"
 sudo borg compact $PATH_TO_BACKUP/Backup/photon
+
+echo "Compacting off-site backup"
+borg compact backup:neutron
