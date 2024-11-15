13ffbe656cdc31c751313c91736d9105703210a6
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2024-11-15 14:23:13 +0100

    updated: photon backup keeps all within 1 day

--- scripts/photon/backup.sh
+++ scripts/photon/backup.sh
@@ -9,13 +9,13 @@ source $HOME/dotfiles/scripts/photon/.env
 # define server and repository
 SERVER=henrik@neutron.lan
 REPO=$PATH_TO_BACKUP/Backup/photon
-DATE=$(date +%Y%m%d)
+DATE=$(date +%Y%m%d-%H%M)
 
 # create borg backup
 sudo borg create --stats --progress --verbose $SERVER:$REPO::$DATE /Users/henrikzujeddeloh/
 
 # prune borg backup
-sudo borg prune --list --stats --keep-daily 7 --keep-weekly 4 --keep-monthly 12 $SERVER:$REPO
+sudo borg prune --list --stats --keep-within=1d --keep-daily 7 --keep-weekly 4 --keep-monthly 12 $SERVER:$REPO
 
 # on the first day of the month, compact borg repo
 if [[ $(date +%d) -eq 01 ]]; then
