a80b9ccb39323189f71523084f278e29377ea4da
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2024-12-21 11:46:56 +0100

    updated: neutron backup pings uptime kuma

--- scripts/neutron/backup.sh
+++ scripts/neutron/backup.sh
@@ -11,7 +11,10 @@ REPO=$PATH_TO_BACKUP/Backup/neutron
 DATE=$(date +%Y%m%d)
 
 # start healthcheck.io ping
-curl -fsS --retry 5 -o /dev/null https://hc-ping.com/$PING_KEY/neutron-backup/start
+#curl -fsS --retry 5 -o /dev/null https://hc-ping.com/$PING_KEY/neutron-backup/start
+
+# start time
+start_time=$(date -u +%s%3N)
 
 # create borg backup
 sudo borg create --stats $REPO::$DATE /home/ /etc/ > $PATH_TO_BACKUP/Backup/logs/${DATE}_borg_backup_neutron.txt 2>&1
@@ -25,4 +28,13 @@ if [[ $(date +%d) -eq 01 ]]; then
 fi
 
 # stop healthchecks.io ping
-curl -fsS --retry 5 -o /dev/null https://hc-ping.com/$PING_KEY/neutron-backup
+#curl -fsS --retry 5 -o /dev/null https://hc-ping.com/$PING_KEY/neutron-backup
+
+# stop time
+end_time=$(date -u +%s%3N)
+
+# duration
+duration=$(($end_time - $start_time))
+
+# ping uptime kuma
+curl "http://uptime.lan/api/push/Iu0zBvn9CG?status=up&msg=OK&ping=$duration"
--- scripts/neutron/borg_offsite.sh
+++ scripts/neutron/borg_offsite.sh
@@ -13,7 +13,10 @@ REPO=neutron
 DATE=$(date +%Y%m%d)
 
 # start healthchecks.io ping
-curl -fsS --retry 5 -o /dev/null https://hc-ping.com/$PING_KEY/offsite-backup/start
+#curl -fsS --retry 5 -o /dev/null https://hc-ping.com/$PING_KEY/offsite-backup/start
+
+# start time
+start_time=$(date -u +%s%3N)
 
 # create borg backup
 borg create --stats $SERVER:$REPO::$DATE $PATH_TO_DATA/Photos $PATH_TO_DATA/Nextcloud/henrik/files $PATH_TO_DATA/Nextcloud/josina/files > $PATH_TO_BACKUP/Backup/logs/${DATE}_borg_offsite.txt 2>&1
@@ -26,5 +29,14 @@ if [[ $(date +%d) -eq 01 ]]; then
     borg compact $SERVER:$REPO >> $PATH_TO_BACKUP/Backup/logs/${DATE}_borg_offsite.txt 2>&1
 fi
 
+# stop time
+end_time=$(date -u +%s%3N)
+
+# duration
+duration=$(($end_time - $start_time))
+
 # stop healthchecks.io ping
-curl -fsS --retry 5 -o /dev/null https://hc-ping.com/$PING_KEY/offsite-backup
+#curl -fsS --retry 5 -o /dev/null https://hc-ping.com/$PING_KEY/offsite-backup
+
+# ping uptime kuma
+http://uptime.lan/api/push/eaYQLb1iKW?status=up&msg=OK&ping=
