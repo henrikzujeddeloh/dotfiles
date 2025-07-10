ec4864cd73d556d1de55a8b10e7b3e3179e29755
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2025-07-10 10:28:35 +0200

    feat: script to check raid consistency

---
+++ scripts/neutron/check_raid_consistency.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+start_time=$(date -u +%s%3N)
+
+GOTIFY_APP_TOKEN="AHgbe0nz1_eLwOG"
+
+RAID_DEVICE="md0"
+
+output=""
+output+="$(sudo /usr/share/mdadm/checkarray --all --idle --quiet 2>&1)"
+if [ $? -ne 0 ]; then
+    curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=RAID Consistency Check Alert" -F "message=$output" -F "priority=10"
+    exit 1;
+fi
+
+# Monitor progress
+while :; do
+    # Check if the consistency check is still running
+    if /usr/share/mdadm/checkarray --all --status | grep -q "check"; then
+        sleep 5 # Wait 5 seconds before refreshing
+    else
+        # Output final RAID details
+        sleep 5
+
+        output+="Final RAID Array Details"$'\n' 
+        output+="$(sudo mdadm --detail /dev/$RAID_DEVICE 2>&1)"$'\n'
+
+        output+="Mismatch Count:"$'\n' 
+        output+="$(cat /sys/block/$RAID_DEVICE/md/mismatch_cnt 2>&1)"$'\n'
+        break
+    fi
+done
+
+
+end_time=$(date -u +%s%3N)
+duration=$((end_time - start_time))
+output+=$(printf "Total duration: %02dh:%02dm:%02ds\n" $((duration/3600000)) $((duration%3600000/60000)) $((duration%60000/1000)))
+
+curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=RAID Consistency Check Complete" -F "message=$output" -F "priority=1"
