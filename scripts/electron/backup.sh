c0b569055043458f62178acd48fa105fccc8e3d2
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2025-07-10 08:55:52 +0200

    feat: added borg check; fix: less output sent to gotify

--- scripts/electron/backup.sh
+++ scripts/electron/backup.sh
@@ -9,18 +9,15 @@ SERVER=henrik@neutron.lan
 DATE=$(date +%Y%m%d)
 
 output="electron backup $(date +%d-%m-%Y)"$'\n'
-output+="----------------------------------------------"$'\n'
 
 output+="$(sudo borg create --stats $SERVER:$REPO::$DATE /home/ /etc/ 2>&1)"$'\n' || { curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=electron backup failed" -F "message=$output" -F "priority=5"; exit 1; }
-output+="----------------------------------------------"$'\n'
 
-output+="$(sudo borg prune --list --stats --keep-daily 7 --keep-weekly 4 --keep-monthly 12 $SERVER:$REPO 2>&1)"$'\n' || { curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=electron backup failed" -F "message=$output" -F "priority=5"; exit 1; }
-output+="----------------------------------------------"$'\n'
+output+="$(sudo borg prune --stats --keep-daily 7 --keep-weekly 4 --keep-monthly 12 $SERVER:$REPO 2>&1)"$'\n' || { curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=electron backup failed" -F "message=$output" -F "priority=5"; exit 1; }
 
 if [[ $(date +%d) -eq 01 ]]; then
     output+="$(sudo borg compact $SERVER:$REPO 2>&1)"$'\n' || { curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=electron backup failed" -F "message=$output" -F "priority=5"; exit 1; }
+    output+="$(sudo borg check --verify-data $SERVER:$REPO 2>&1)"$'\n' || { curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=electron backup failed" -F "message=$output" -F "priority=5"; exit 1; }
 fi
-output+="----------------------------------------------"$'\n'
 
 end_time=$(date -u +%s%3N)
 duration=$((end_time - start_time))
--- scripts/neutron/borg_offsite.sh
+++ scripts/neutron/borg_offsite.sh
@@ -9,18 +9,15 @@ REPO=neutron
 DATE=$(date +%Y%m%d)
 
 output="offsite backup $(date +%d-%m-%Y)"$'\n'
-output+="----------------------------------------------"$'\n'
 
 output+="$(borg create --stats $SERVER:$REPO::$DATE $PATH_TO_DATA/Photos $PATH_TO_DATA/Nextcloud/henrik/files $PATH_TO_DATA/Nextcloud/josina/files $PATH_TO_DATA/Immich/library 2>&1)"$'\n' || { curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=offsite backup failed" -F "message=$output" -F "priority=5"; exit 1; }
-output+="----------------------------------------------"$'\n'
 
-output+="$(borg prune --list --stats --keep-daily 7 --keep-weekly 4 --keep-monthly 12 $SERVER:$REPO 2>&1)"$'\n' || { curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=offsite backup failed" -F "message=$output" -F "priority=5"; exit 1; }
-output+="----------------------------------------------"$'\n'
+output+="$(borg prune --stats --keep-daily 7 --keep-weekly 4 --keep-monthly 12 $SERVER:$REPO 2>&1)"$'\n' || { curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=offsite backup failed" -F "message=$output" -F "priority=5"; exit 1; }
 
 if [[ $(date +%d) -eq 01 ]]; then
     output+="$(borg compact $SERVER:$REPO 2>&1)"$'\n' || { curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=offsite backup failed" -F "message=$output" -F "priority=5"; exit 1; }
+    output+="$(borg check --verify-data $SERVER:$REPO 2>&1)"$'\n' || { curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=offsite backup failed" -F "message=$output" -F "priority=5"; exit 1; }
 fi
-output+="----------------------------------------------"$'\n'
 
 end_time=$(date -u +%s%3N)
 duration=$((end_time - start_time))
