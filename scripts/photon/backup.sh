807b2a2edbd3b1a1b05c63351bfec48f91acd7c6
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2025-07-17 19:20:36 +0200

    feat: added compression to photon backup

--- scripts/photon/backup.sh
+++ scripts/photon/backup.sh
@@ -7,7 +7,7 @@ REPO=$PATH_TO_BACKUP/photon
 DATE=$(date +%Y%m%d-%H%M)
 
 # create borg backup
-sudo borg create --stats --progress --verbose $SERVER:$REPO::$DATE /Users/henrikzujeddeloh/ --exclude '/Users/henrikzujeddeloh/Library/'
+sudo borg create --progress --stats --compression zstd,6 $SERVER:$REPO::$DATE /Users/henrikzujeddeloh/ --exclude '/Users/henrikzujeddeloh/Library/'
 
 # prune borg backup
 sudo borg prune --list --stats --keep-within=1d --keep-daily 7 --keep-weekly 4 --keep-monthly 12 $SERVER:$REPO
