683e6b8a22a26725c75b96c60f12ababb87bb7b9
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2025-07-18 13:31:17 +0200

    refactor: alacritty install script improved for robustness and installs app if not already

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,19 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# install alacritty and config
 
 # check if alacritty is installed
-if ! which alacritty &> /dev/null
-then
-    echo "alacritty is not installed. Please install alacritty first."
-    exit 1
+if [ ! which alacritty &> /dev/null ]; then
+    echo "zsh is not installed. Installing..."
+    if [[ $(uname) == "Linux" ]]; then
+        sudo apt install alacritty
+    elif [[ $(uname) == "Darwin" ]]; then
+        brew install alacritty
+    else
+        echo "[ERROR] Unknown OS!"
+        exit 1
+    fi
 fi
+
+# create config folder
+mkdir -p "$XDG_CONFIG_HOME/alacritty"
 
 # remove existing alacritty config
-rm -f $CONFIGS/alacritty/alacritty.toml
+rm -f "$XDG_CONFIG_HOME/alacritty/alacritty.toml"
 
-# check if alacritty config directory exists
-if [ ! -d $CONFIGS/alacritty ]; then
-  mkdir -p $CONFIGS/alacritty;
-fi
 # add symlinks
-ln -s $DOTFILES/alacritty/alacritty.toml $CONFIGS/alacritty/alacritty.toml
-echo "Created symlink to alacritty config at $CONFIGS/alacritty/alacritty.toml"
+echo "Creating symlink for alacritty.toml"
+ln -s $DOTFILES/alacritty/alacritty.toml $XDG_CONFIG_HOME/alacritty/alacritty.toml
+
+echo "[SUCCESS] alacritty and config installed."
