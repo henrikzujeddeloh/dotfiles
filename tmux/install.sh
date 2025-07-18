b96c081ebe9ebb87dd6a6eedd37f2cb712c91338
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2025-07-18 15:36:33 +0200

    refactor: tmux install script improved and transition to XDG format

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,15 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# install tmux and config
 
 # check if tmux is installed
-if ! which tmux &> /dev/null
-then
-    echo "tmux is not installed. Please install tmux first."
-    exit 1
+if [ ! which tmux &> /dev/null ]; then
+    echo "tmux is not installed. Installing..."
+    if [[ $(uname) == "Linux" ]]; then
+        sudo apt install tmux
+    elif [[ $(uname) == "Darwin" ]]; then
+        brew install tmux
+    else
+        echo "[ERROR] Unknown OS!"
+        exit 1
+    fi
 fi
 
-# remove existing tmux config
-rm -f ~/.tmux.conf
+# create config folder
+mkdir -p "$XDG_CONFIG_HOME/tmux"
 
-# create symlink to tmux config
-ln -s $DOTFILES/tmux/.tmux.conf $HOME/.tmux.conf
-echo "Created symlink to tmux config at $HOME/.tmux.conf"
+# remove existing tmux config
+rm -f "$XDG_CONFIG_HOME/tmux/tmux.conf"
+
+# add symlinks
+echo "Creating symlink for tmux.conf"
+ln -s $DOTFILES/tmux/tmux.conf $XDG_CONFIG_HOME/tmux/tmux.conf
+
+echo "[SUCCESS] tmux and config installed."
