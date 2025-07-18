72da08be1ee23043ff62711e372596501cbabc5f
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2025-07-18 15:45:18 +0200

    feature: electron crontab moved to scripts

--- scripts/electron/backup.sh
+++ scripts/electron/backup.sh
@@ -2,25 +2,22 @@
 
 start_time=$(date -u +%s%3N)
 
-GOTIFY_APP_TOKEN="AHgbe0nz1_eLwOG"
-PATH_TO_BACKUP=/srv/data/Backup
-REPO=$PATH_TO_BACKUP/electron
-SERVER=henrik@neutron.lan
-DATE=$(date +%Y%m%d)
+repo=$BACKUP_DIR/electron
+archive=$(date +%Y%m%d)
 
 output="electron backup $(date +%d-%m-%Y)"$'\n'
 
-output+="$(sudo borg create --stats --compression zstd,6 $SERVER:$REPO::$DATE /home/ /etc/ 2>&1)"$'\n' || { curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=electron backup failed" -F "message=$output" -F "priority=5"; exit 1; }
+output+="$(sudo borg create --stats --compression zstd,6 $BACKUP_SERVER:$repo::$archive /home/ /etc/ 2>&1)"$'\n' || { curl -sSf -o /dev/null "$GOTIFY_URL/message?token=$GOTIFY_API" -F "title=electron backup failed" -F "message=$output" -F "priority=5"; exit 1; }
 
-output+="$(sudo borg prune --stats --keep-daily 7 --keep-weekly 4 --keep-monthly 12 $SERVER:$REPO 2>&1)"$'\n' || { curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=electron backup failed" -F "message=$output" -F "priority=5"; exit 1; }
+output+="$(sudo borg prune --stats --keep-daily 7 --keep-weekly 4 --keep-monthly 12 $BACKUP_SERVER:$repo 2>&1)"$'\n' || { curl -sSf -o /dev/null "$GOTIFY_URL/message?token=$GOTIFY_API" -F "title=electron backup failed" -F "message=$output" -F "priority=5"; exit 1; }
 
 if [[ $(date +%d) -eq 01 ]]; then
-    output+="$(sudo borg compact $SERVER:$REPO 2>&1)"$'\n' || { curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=electron backup failed" -F "message=$output" -F "priority=5"; exit 1; }
-    output+="$(sudo borg check --verify-data $SERVER:$REPO 2>&1)"$'\n' || { curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=electron backup failed" -F "message=$output" -F "priority=5"; exit 1; }
+    output+="$(sudo borg compact $BACKUP_SERVER:$repo 2>&1)"$'\n' || { curl -sSf -o /dev/null "$GOTIFY_URL/message?token=$GOTIFY_API" -F "title=electron backup failed" -F "message=$output" -F "priority=5"; exit 1; }
+    output+="$(sudo borg check --verify-data $BACUKP_SERVER:$repo 2>&1)"$'\n' || { curl -sSf -o /dev/null "$GOTIFY_URL/message?token=$GOTIFY_API" -F "title=electron backup failed" -F "message=$output" -F "priority=5"; exit 1; }
 fi
 
 end_time=$(date -u +%s%3N)
 duration=$((end_time - start_time))
 output+=$(printf "Total duration: %02dh:%02dm:%02ds\n" $((duration/3600000)) $((duration%3600000/60000)) $((duration%60000/1000)))
 
-curl -sSf -o /dev/null "http://gotify.lan/message?token=$GOTIFY_APP_TOKEN" -F "title=electron backup successful" -F "message=$output" -F "priority=1"
+curl -sSf -o /dev/null "$GOTIFY_URL/message?token=$GOTIFY_API" -F "title=electron backup successful" -F "message=$output" -F "priority=1"
