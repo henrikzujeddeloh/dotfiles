dba1125d902bd9934330e6c81febd9df379a482a
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2025-07-18 15:37:20 +0200

    feat: added fastfetch config

---
+++ fastfetch/install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# install fastfetch and config
+
+# check if fastfetch is installed
+if [ ! which fastfetch &> /dev/null ]; then
+    echo "fastfetch is not installed. Installing..."
+    if [[ $(uname) == "Linux" ]]; then
+        sudo apt install fastfetch
+    elif [[ $(uname) == "Darwin" ]]; then
+        brew install fastfetch
+    else
+        echo "[ERROR] Unknown OS!"
+        exit 1
+    fi
+fi
+
+# create config folder
+mkdir -p "$XDG_CONFIG_HOME/fastfetch"
+
+# remove existing fastfetch config
+rm -f "$XDG_CONFIG_HOME/fastfetch/config.jsonc"
+
+# add symlinks
+echo "Creating symlink for config.jsonc"
+ln -s $DOTFILES/fastfetch/config.jsonc $XDG_CONFIG_HOME/fastfetch/config.jsonc
+
+echo "[SUCCESS] fastfetch and config installed."
