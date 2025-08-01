cb8f8b68fc19edf83e5acbe704a2959dcb3f570b
Henrik zu Jeddeloh <henrik.zujeddeloh@protonmail.com>
2025-08-01 11:11:02 +0200

    fix: fd | fzf works in new_tmux_window.sh on all OS

--- tmux/fzf-new-window.sh
+++ tmux/fzf-new-window.sh
@@ -1,8 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Use fzf to select a directory
+# define fd command based on OS (run in bash so not done by "alias.zsh")
 # DIRECTORY=$(find ~ -type d | fzf --prompt="Select a directory: " --preview="echo {}" --preview-window=up:30%:wrap)
-DIRECTORY=$(fd . $HOME --type directory --hidden --exclude .git | fzf)
+if [[ $(uname) == "Linux" ]]; then
+    DIRECTORY=$(fdfind . $HOME --type directory --hidden --exclude .git | fzf)
+elif [[ $(uname) == "Darwin" ]]; then
+    DIRECTORY=$(fd . $HOME --type directory --hidden --exclude .git | fzf)
+else
+    echo "Unknown OS!"
+fi
 
 # If a directory is selected, create a new window in that directory
 if [ -n "$DIRECTORY" ]; then
